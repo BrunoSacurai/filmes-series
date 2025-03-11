a47c3062a377dc08f05822638b02c866ffad2e73
Felipe Dumont <50582912+Felipe-Dumont@users.noreply.github.com>
2025-03-11 20:50:36 -0300

    criado o metodo de rendereziar a lista, e a tela de cadastro de review (#3)

--- js/filmes.js
+++ js/filmes.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", async function () {
     const token = localStorage.getItem('authToken')
+    const list = document.getElementById('list')
+    const createMovie = document.getElementById('create')
 
     async function searchMovieList(){
         
@@ -12,10 +14,30 @@ document.addEventListener("DOMContentLoaded", async function () {
             });
     
             const movies = await response.json()
+
+            movies.forEach(movie => renderMovie(movie))
         } catch (error) {
             window.alert(error.mensage)
         }
     }
+
+    function renderMovie(movie) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div>${movie.titulo} - ${movie.ano}</div>
+            <div>
+                <button onclick="viewMovie(${movie.id})">Ver</button>
+                <button onclick="edit(${movie.id})">Editar</button>
+                <button onclick="deleteMovie(${movie.id})">Excluir</button>
+            </div>
+        `;
+
+        list.appendChild(div);
+    }
+
+    createMovie.addEventListener("click", () => {
+        window.location.href = 'criar.html'
+    })
 
     await searchMovieList()
 }) 
